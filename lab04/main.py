85b4cbd7a30857d9b3fed3ad18740045e9cd09b8
Oleksandr Leskiv <oleksandr.leskiv@digia.co>
2020-02-04 17:54:04 +0200

    lab04. Animation

--- lab04/main.py
+++ lab04/main.py
@@ -1,47 +1,54 @@
 import cv2
 import numpy as np
-import colorsys
 
 width = 600
 height = 600
-canvas = np.zeros((height, width, 3), dtype=np.uint8)
 
 
-def dots(p, q, max_n, xn=0, yn=0, n=0):
-    if n > max_n:
-        return
-    xn1 = xn ** 2 - yn ** 2 + p
-    yn1 = 2 * xn * yn + q
-    yield xn1, yn1
-    for new_dot in dots(p, q, max_n, xn1, yn1, n + 1):
-        yield new_dot
+def fractal(pmin, pmax, qmin, qmax,
+            max_iterations=122, infinity_border=10):
+    canvas = np.zeros((height, width, 3), dtype=np.uint8)
+    p, q = np.mgrid[pmin:pmax:(width * 1j), qmin:qmax:(height * 1j)]
+
+    c = p + 1j * q
+    z = np.zeros_like(c)
+
+    for k in range(max_iterations):
+        z = z ** 2 + c
+
+        mask = (np.abs(z) > infinity_border) & (np.add.reduce(canvas, axis=2) == 0)
+
+        canvas[mask] = (k * 2, 150, 255)
+        z[mask] = np.nan
+    return canvas
 
 
-pmin, pmax, qmin, qmax = -3, 3, -2, 2
-max_iterations = 122
-infinity_border = 10
+max_frames = 300
+max_zoom = 200
+pmin, pmax, qmin, qmax = -2.5, 1.5, -2, 2
 
-for ip, p in enumerate(np.linspace(pmin, pmax, width)):
-    for iq, q in enumerate(np.linspace(qmin, qmax, height)):
-        c = p + 1j * q
 
-        z = 0
-        for k in range(max_iterations):
-            z = z ** 2 + c
+def render_frame(i):
+    print(f'\r{i + 1}/{max_frames}', end='')
 
-            if abs(z) > infinity_border:
-                canvas[ip, iq] = (k * 2, 150, 255)
-                break
+    p_center, q_center = -0.793191078177363, 0.16093721735804
+    zoom = (i / max_frames * 2) ** 3 * max_zoom + 1
+    scalefactor = 1 / zoom
 
-cv2.imshow('fractal', canvas)
-cv2.waitKey(1)
+    pmin_ = (pmin - p_center) * scalefactor + p_center
+    qmin_ = (qmin - q_center) * scalefactor + q_center
+    pmax_ = (pmax - p_center) * scalefactor + p_center
+    qmax_ = (qmax - q_center) * scalefactor + q_center
 
-# for (x, y) in dots(beautiful_q, beautiful_p, 200):
-#     print(x, y)
-# canvas_x = int(width * (x + 0.5) / 100)
-# canvas_y = int(height * (y + 0.5) / 100)
-# canvas[canvas_y, canvas_x] = (255, 255, 255)
+    frame = fractal(pmin_, pmax_, qmin_, qmax_)
+    return cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)
 
-output = cv2.cvtColor(canvas, cv2.COLOR_HSV2BGR)
-cv2.imshow('fractal', output)
-cv2.waitKey(0)
+
+frames = [render_frame(i) for i in range(max_frames)]
+while True:
+    for f in frames:
+        cv2.imshow('fractal', f)
+        cv2.waitKey(2)
+    for f in reversed(frames):
+        cv2.imshow('fractal', f)
+        cv2.waitKey(2)
